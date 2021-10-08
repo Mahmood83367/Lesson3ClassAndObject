4050214ab3f4356ec8fa4f0071acb151bada6f72
Mahmood83367 <mahmood83367@gmail.com>
2021-10-08 17:26:01 +0300

    Lesson 3 class and object

---
+++ Employee.py
@@ -0,0 +1,6 @@
+class Employee:
+ def __init__(self, name, age, department, is_manager):
+  self.name = name
+  self.age = age
+  self.department = department
+  self.is_manager = is_manager
---
+++ app.py
@@ -0,0 +1,7 @@
+from Employee import Employee
+employee1 = Employee("mahmood", 27, "Fullstack devlopment Bootcamp", False)
+employee2 = Employee("Layth", 30 , "GIZ", True)
+
+print ("Employee Name",employee1.name, "Employee age",employee1.age, "Employee Department",employee1.department, "is Manager?",employee1.is_manager)
+
+print ("Employee Name",employee2.name, "Employee age",employee2.age, "Employee Department",employee2.department, "is Manager?",employee2.is_manager)
